f95cc47ecd324820d6c8bdacc4f0b8424fe52c67
mong83 <06007sk@gmail.com>
2020-04-07 07:53:13 +0900

    DispatcherServlet - 계층형으로 구성

--- src/main/java/com/kyk/AppConfig.java
+++ src/main/java/com/kyk/AppConfig.java
@@ -2,8 +2,9 @@ package com.kyk;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan
+@ComponentScan(excludeFilters = @ComponentScan.Filter(Controller.class))
 public class AppConfig {
 }
