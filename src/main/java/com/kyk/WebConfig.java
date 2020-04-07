ba432f86f834f4098fca369d15fc6654d681ed20
mong83 <06007sk@gmail.com>
2020-04-07 09:14:52 +0900

    DispatcherServlet - 단일구성(최근 사용방식)

---
+++ src/main/java/com/kyk/WebConfig.java
@@ -0,0 +1,11 @@
+package com.kyk;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+@ComponentScan
+public class WebConfig {
+
+}
